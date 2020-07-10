e751f8f2ba22a737de5f05b5b1bebf0e14b40a22
Odd Stråbø <oddstr13@openshell.no>
2020-07-10 20:10:57 +0200

    Things and stuff

--- Jellyfin.Plugin.YoutubeMetadata/Providers/YoutubeLocalImageProvider.cs
+++ Jellyfin.Plugin.YoutubeMetadata/Providers/YoutubeLocalImageProvider.cs
@@ -1,43 +1,84 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Collections.Generic;
+//using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
+
 using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Controller.Entities;
-using MediaBrowser.Model.IO;
 using MediaBrowser.Controller.Entities.Movies;
-using Microsoft.Extensions.Logging;
+using MediaBrowser.Controller.Drawing;
+using MediaBrowser.Model.Drawing;
+using MediaBrowser.Model.IO;
+using MediaBrowser.Model.Entities;
 
 namespace Jellyfin.Plugin.YoutubeMetadata.Providers
 {
-    public class YoutubeLocalImageProvider : ILocalImageFileProvider, IHasOrder
+    public class YoutubeLocalImageProvider : ILocalImageProvider, IHasOrder
     {
         private readonly IFileSystem _fileSystem;
-        private readonly ILogger _logger;
+        private readonly ILogger<YoutubeLocalImageProvider> _logger;
+        private readonly IImageProcessor _imageProcessor;
 
-        public YoutubeLocalImageProvider(IServerConfigurationManager config, IFileSystem fileSystem, ILogger logger)
+        public YoutubeLocalImageProvider(IServerConfigurationManager config, IFileSystem fileSystem, ILogger<YoutubeLocalImageProvider> logger, IImageProcessor imageProcessor)
         {
             _fileSystem = fileSystem;
             _logger = logger;
+            _imageProcessor = imageProcessor;
         }
         // This does not look neccesary?
         public string Name => "YoutubeMetadata";
         public int Order => 1;
+
+        //public const string YTDL_THUMBNAILS_RE_STR = @"(_[0-9])?\.(jpe?g|webp|png|gif|tiff?)$";
+        //private Regex _thumbnailsRe = new Regex(YTDL_THUMBNAILS_RE_STR, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.ExplicitCapture);
+        
         public List<LocalImageInfo> GetImages(BaseItem item, IDirectoryService directoryService)
         {
             _logger.LogInformation(item.Path);
             var list = new List<LocalImageInfo>();
-            var filename = item.FileNameWithoutExtension + ".jpg";
-            var fullpath = Path.Combine(item.ContainingFolderPath, filename);
 
-            var localimg = new LocalImageInfo();
-            var fileInfo = _fileSystem.GetFileSystemInfo(fullpath);
-            if (File.Exists(fileInfo.FullName))
-            {
-                localimg.FileInfo = fileInfo;
-                list.Add(localimg);
+            var files = _fileSystem.GetFiles(item.ContainingFolderPath);
+
+            LocalImageInfo largestLocalImg = null;
+            ImageDimensions largestSize = new ImageDimensions(0, 0);
+
+            //_logger.LogDebug("item.FileNameWithoutExtension: {0}", item.FileNameWithoutExtension);
+            foreach (FileSystemMetadata file in files) {
+                //_logger.LogDebug("file.Name: {0}", file.Name);
+
+                if (file.Name.StartsWith(item.FileNameWithoutExtension)) {
+                    //_logger.LogDebug("File is related!");
+                    //_logger.LogDebug(file.Extension);
+
+                    //if (_thumbnailsRe.IsMatch(file.Name)) {
+                    if (_imageProcessor.SupportedInputFormats.Contains(file.Extension.ToLower().TrimStart('.'))) {
+                        //_logger.LogDebug("Regex matches!");
+                        //_logger.LogDebug("Is image!");
+
+                        ImageDimensions size = _imageProcessor.GetImageDimensions(file.FullName);
+
+                        if (largestSize.Width < size.Width) {
+                            //_logger.LogDebug("Image is larger!");
+                            largestLocalImg = new LocalImageInfo {
+                                FileInfo = file,
+                                Type = ImageType.Thumb
+                            };
+                            largestSize = size;
+                        }
+                    }
+                }
             }
+
+            if (!(largestLocalImg is null)) {
+                _logger.LogDebug("Picking {0} with dimensions {1}x{2}", largestLocalImg.FileInfo.Name, largestSize.Width, largestSize.Height);
+                list.Add(largestLocalImg);
+            }
+
             return list;
         }
+
         public bool Supports(BaseItem item)
             => item is Movie;
     }
